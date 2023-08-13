342ab046f1e14c4250945128caeac828033752e5
EmranSWE <mdemran.swe@gmail.com>
2023-08-13 11:35:38 +0600

    [DONE] table to table relation

--- index.ts
+++ index.ts
@@ -2,15 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 async function main() {
-  const getAllUsers = await prisma.user.findMany();
-  console.log(getAllUsers);
-  //   const postUser = await prisma.user.create({
-  //     data: {
-  //       email: "imran@gmail.com",
-  //       name: "imran",
-  //     },
-  //   });
-  //   console.log(postUser);
+  //   const getAllUsers = await prisma.user.findMany();
+  //   console.log(getAllUsers);
+  const postUser = await prisma.user.create({
+    data: {
+      email: "imran1@gmail.com",
+      name: "imran",
+      role: "",
+    },
+  });
+  console.log(postUser);
 }
 
 main();
